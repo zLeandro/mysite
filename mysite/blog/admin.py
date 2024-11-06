561580ee4164490fb00d740dcd64b461cbb5fcad
Leandro Walter <leandromarqueswalter@gmail.com>
2024-11-06 17:34:52 -0300

    Resolvendo conflitos git

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import Post
 
+
 class PostAdmin(admin.ModelAdmin):
     list_display = ("title", "slug", "status", "created_at")
     list_filter = ("status",)
