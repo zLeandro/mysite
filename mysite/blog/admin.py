bd902893788d8019d218553cd3642b5591ae0874
Leandro Walter <leandromarqueswalter@gmail.com>
2024-11-07 17:46:39 -0300

    erro

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -1,16 +1,12 @@
+# blog/admin.py
 from django.contrib import admin
-from .models import Post
+from .models.post import Post
+
 
 class PostAdmin(admin.ModelAdmin):
-    list_display = ("title", "slug", "status", "get_created_at")
+    list_display = ("title", "slug", "status", "created_on", "updated_on")
     list_filter = ("status",)
     search_fields = ["title", "content"]
     prepopulated_fields = {"slug": ("title",)}
-
-    def get_created_at(self, obj):
-        return obj.created_at
-
-    get_created_at.admin_order_field = 'created_at'
-    get_created_at.short_description = 'Created At'
 
 admin.site.register(Post, PostAdmin)
--- mysite/blog/models/post.py
+++ mysite/blog/models/post.py
@@ -13,11 +13,11 @@ class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_post')
     updated_on = models.DateTimeField(auto_now=True)
     content = models.TextField()
-    created_on = models.DateTimeField(auto_now_add =True)
+    created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
 
     class Meta:
         ordering = ['-created_on']
-       
+
     def __str__(self):
         return self.title
